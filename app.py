cc4afb85647e7ebdfcae1ef7a9a536c8c1411412
BryanChan0528 <84577153+BryanChan0528@users.noreply.github.com>
2025-01-03 14:12:21 +0800

    Update app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model_path = "model/Random Forest_model.pkl"
+scaler_path = "model/scaler.pkl"
+
+loaded_model = joblib.load(model_path)
+loaded_scaler = joblib.load(scaler_path)
+
+# Streamlit App
+st.title("Obesity Prediction")
+
+st.write("Enter the input values for the fields:")
+
+# Input fields for the user
+fields = ['emotional_eating', 'water_intake', 'freq_high_calorie_food', 'num_meals_per_day', 'height_meters', 'Age', 'family_history_overweight']
+
+input_data = []
+for field in fields:
+    value = st.number_input(f"{field}", min_value=1.0, step=1e-4, format="%.3f")
+    input_data.append(value)
+
+# Predict button
+if st.button("Predict Obesity"):
+    # Scale the input data
+    new_data = np.array(input_data).reshape(1, -1)
+    new_data_scaled = loaded_scaler.transform(new_data)
+
+    # Predict obesity
+    obesity = loaded_model.predict(new_data_scaled)
+    st.success(f"Predicted Obesity: {obesity[0]}")
